e50b5f03ad74a75eb1ecc0544f0f67d585b48243
AnnittaCele <zibuyilecelea@gmail.com>
2023-07-05 10:11:59 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts_recursion - Print a string followed by a new line
- * @s: the string to print
+ * @s: the string tp print
  *
  * Return: Nothing.
  */
@@ -10,7 +10,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\0');
+		_putchar('\n');
 		return;
 	}
 
